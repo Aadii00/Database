c5d2bfac78b79ad5700ed72c14094830aa2cca5d
Aditya Singh <112930600+Aadii00@users.noreply.github.com>
2022-09-07 10:04:41 +0530

    Delete wellfargotables.sql

--- wellfargotables.sql
+++
@@ -1,9 +0,0 @@
-Create Table branch (
-    bid varchar(6) NOT NULL,
-    baddress varchar(30) NULL,
-    bcity varchar(30) NULL,
- CONSTRAINT [branch_bid_pk] PRIMARY KEY CLUSTERED
-(
-    [bid] ASC
-)
-) ON [PRIMARY]
